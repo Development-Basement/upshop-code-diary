272b5c809fd1b4a653de66eced310d224dd821f5
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2023-02-18 19:47:48 +0100

    configure ESLint & Prettier, VS Code settings also fix most lint issues and formatted all files

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-
 import { env } from "../env.mjs";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
