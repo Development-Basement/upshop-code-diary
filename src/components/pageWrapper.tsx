120ee4c2a2ced07e23130049bf41984c695a45df
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2023-03-02 20:44:14 +0100

    fix overflow in PageWrapper by adding a small padding

--- src/components/pageWrapper.tsx
+++ src/components/pageWrapper.tsx
@@ -10,7 +10,7 @@ const PageWrapper: FC<Children> = ({ children, showHeader }) => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-2 overflow-hidden bg-gradient-to-b from-bgdark1 to-bgdark3">
       {showHeader && <Header />}
-      <div className="flex h-full max-h-full w-[38rem] flex-col items-center justify-center overflow-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-600 scrollbar-thumb-rounded-full">
+      <div className="flex h-full max-h-full w-[40rem] flex-col items-center justify-center overflow-auto px-3 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-600 scrollbar-thumb-rounded-full">
         {children}
       </div>
     </div>
